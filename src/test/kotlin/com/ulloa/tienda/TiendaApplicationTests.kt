61f9f0582d357774efc023d4502a7ebb984567a0
Karen <ksulloa@sudamericano.edu.ec>
2021-10-25 18:27:53 -0500

    first commit

---
+++ src/test/kotlin/com/ulloa/tienda/TiendaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ulloa.tienda
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TiendaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
